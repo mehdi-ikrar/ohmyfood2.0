cc95c40cb4801018b05a4b35eb415838ed47cfbe
mehdi-ikrar <ikrar.mehdi@gmail.com>
2025-09-04 00:29:40 +0200

    Mise à jour de la configuration de Sequelize pour utiliser les variables d'environnement DB_NAME, DB_USER et DB_PASS, et activation des timestamps.

--- back/models/sequelize_client.js
+++ back/models/sequelize_client.js
@@ -1,13 +1,18 @@
 import "dotenv/config";
 import { Sequelize } from "sequelize";
 
-
-export const sequelize = new Sequelize(process.env.PG_URL, {
-  dialect: "postgres",
-  define: {
-    timestamps: false
+export const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    dialect: "postgres",
+    host: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+    define: {
+      timestamps: true
+    }
   }
-});
+);
 
 
 
