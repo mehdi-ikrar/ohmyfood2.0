8f2a67f79ef35ef12b773f6c056455fd7ee6393e
esportaubervilliers <esportaubervilliers@gmail.com>
2026-09-12 12:21:03 +0200

    changement image oriental

--- back/data/restaurant.js
+++ back/data/restaurant.js
@@ -19,7 +19,7 @@ export const restaurants = [
   },
   {
     id: 3,
-    name: "Saveurs d'Orient",
+    name: "Saveurs d'Orientale",
     image: "Saveurs_dorient.jpg",
     description: "Voyage culinaire au Moyen-Orient avec des plats traditionnels et épicés.",
     address: "25 avenue Habib Bourguiba",
